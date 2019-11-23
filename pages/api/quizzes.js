aac899216b1e4c8fc1fc251b39595c06c2c8bb81
Ilia Potuzhnov <ipotuzhnov@gmail.com>
2019-11-22 21:45:07 -0800

    Our first quiz!

---
+++ pages/api/quizzes.js
@@ -0,0 +1,28 @@
+export default (req, res) => {
+  let body
+
+  if (req.method === 'POST') {
+    // TODO: save the quiz results here
+    body = { answer: 'Nextjs' }
+  }
+  else {
+    // TODO: return the quiz questions here
+    body = {
+      questions: [
+        {
+          text: '1. How foamy are you?',
+          answers: [
+            {text: 'No foam at all'},
+            {text: 'A little bit of bubbles'},
+            {text: 'I am in the sweet spot'},
+            {text: 'Pure foam'},
+          ],
+        },
+      ]
+    }
+  }
+
+  res.setHeader('Content-Type', 'application/json')
+  res.statusCode = 200
+  res.end(JSON.stringify(body))
+}
