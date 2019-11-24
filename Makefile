1729aa9f0951087f996fe8080733dcddf135ff17
Richard Marmorstein <richard.marmorstein@gmail.com>
2019-11-23 19:20:48 -0500

    Augment quizzes with results

---
+++ Makefile
@@ -0,0 +1,7 @@
+quizzes.json:
+	node fetchLists.js quizzes > quizzes.json
+littlequizzes.json: quizzes.json
+	cat quizzes.json | jq 'def keeper(x): [(x | contains("cryptid")), (x | contains("industrial disasters")), (x | contains("poker hands"))] | any; [.[] | select(keeper(.title))'] > littlequizzes.json
+littlequizzeswithpossibleresults.json: littlequizzes.json fetchLists.js
+	node fetchLists.js quizzeswithpossibleresults > littlequizzeswithpossibleresults.json
+
--- fetchLists.js
+++ fetchLists.js
@@ -3,48 +3,105 @@ const { promisify } = require("util");
 const parseString = promisify(require("xml2js").parseString);
 const fs = require("fs");
 
-const query = `
-SELECT ?Wikimedia_list_article ?Wikimedia_list_articleLabel WHERE {
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-  OPTIONAL {  }
-  ?Wikimedia_list_article wdt:P31 wd:Q13406463.
-}
-`;
+const action = process.argv[2];
 
-const main = async () => {
-  let res;
+const withCache = async (name, fetchText) => {
   try {
-    res = fs.readFileSync("allLists.xml").toString();
+    return fs.readFileSync(name).toString();
   } catch (e) {
-    console.log("could not find cache in allLists.xml, sending query");
-    res = await fetch(
-      "https://query.wikidata.org/sparql?query=" + encodeURIComponent(query)
-    );
-    fs.writeFileSync("allLists.xml", await res.text());
+    console.error(`could not find cache in ${cacheName}, sending query`);
+    const res = await fetchText();
+    fs.writeFileSync(name);
+    return res;
   }
+};
 
-  // const result = (await parseString(res)).sparql.results[0].result;
-  // const response = await fetch(
-  //   "https://query.wikidata.org/sparql?query=" + encodeURIComponent(query)
-  // );
-  const result = await parseString(res);
+const fromWikidata = async (query, cache) => {
+  const f = async () => {
+    return await (
+      await fetch(
+        "https://query.wikidata.org/sparql?query=" + encodeURIComponent(query)
+      )
+    ).text();
+  };
+
+  const text = cache ? await withCache(cache, f) : await f();
+  return await parseString(text);
+};
+
+const idFromURI = uri => uri.split("/").slice(-1)[0];
+
+const fetchPossibleQuizResults = async listID => {
+  const query = `
+SELECT distinct ?listItem ?listItemLabel {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  ?listItem wdt:P31 ?y.
+  ?y wdt:P910 ?z.
+  ?z wdt:P1753 wd:${listID}.
+}
+LIMIT 1000
+`;
+  const result = await fromWikidata(query);
 
   const results = result.sparql.results.reduce((acc, r) => {
     return [...acc, ...r.result];
   }, []);
-  const list = results
+  const list = results.map(x => {
+    const uri = x.binding.filter(y => y["uri"])[0].uri[0];
+    const title = x.binding.filter(y => y["literal"])[0].literal[0]["_"];
+    return { uri, title, id: idFromURI(uri) };
+  });
+  return list;
+};
+
+const augmentQuizWithResults = async quiz => {
+  return { ...quiz, possible_results: await fetchPossibleQuizResults(quiz.id) };
+};
+
+const fetchQuizList = async () => {
+  const query = `
+SELECT distinct ?list ?listLabel {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  # ?listArticle wdt:P31 wd:Q13406463.
+  # ?listArticle wdt:P3921 ?sparql.
+  ?listItem wdt:P31 ?y.
+  ?y wdt:P910 ?z.
+  ?z wdt:P1753 ?list.
+ }
+LIMIT 1000
+`;
+  const json = await fromWikidata(query, "allLists.xml");
+
+  const results = json.sparql.results.reduce((acc, r) => {
+    return [...acc, ...r.result];
+  }, []);
+
+  return results
     .map(x => {
       const uri = x.binding.filter(y => y["uri"])[0].uri[0];
       const title = x.binding.filter(y => y["literal"])[0].literal[0]["_"];
-      return { uri, title };
+      return { uri, title, id: idFromURI(uri) };
     })
     .filter(
       x =>
         x.title &&
         (x.title.indexOf("List") !== -1 || x.title.indexOf("list") !== -1)
     );
-  console.log(JSON.stringify(list, null, 2));
 };
+
+const prettyPrint = x => console.log(JSON.stringify(x, null, 2));
+
+const main = async () => {
+  if (action === "quizzes") {
+    prettyPrint(await fetchQuizList());
+  }
+  if (action === "quizzeswithpossibleresults") {
+    const quizzes = JSON.parse(fs.readFileSync("littlequizzes.json"));
+
+    prettyPrint(await Promise.all(quizzes.map(augmentQuizWithResults)));
+  }
+};
+
 main()
   .then()
   .catch(e => {
