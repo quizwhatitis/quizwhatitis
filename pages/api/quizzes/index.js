6a45c613394922faf3920acc69422fa08f6269e5
Ilia Potuzhnov <ipotuzhnov@gmail.com>
2019-11-23 16:17:04 -0800

    Revamp the home page - add quiz links

--- pages/api/quizzes/index.js
+++ pages/api/quizzes/index.js
@@ -1,13 +1,14 @@
+import quizzes from '../../../littlequizzes'
+
 export default (req, res) => {
   // TODO: return a list of quizzes here
   const body = {
-    quizzes: [
-      {
-        id: 1,
-        displayName: 'What soap are you?',
-      },
-    ]
-  };
+    quizzes: quizzes.map(({title, uri}) => ({
+      id: uri.split('/').pop(),
+      uri,
+      title,
+    }))
+  }
 
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
--- pages/index.js
+++ pages/index.js
@@ -2,93 +2,113 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Quiz what it is</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+function Home ({quizzes}) {
+  const quizLinks = quizzes.map(({title, id}) => (
+    <li key={id}>
+      <a href={getQuizUri({id})}>
+        {getQuizName({title})}
+      </a>
+    </li>
+  ))
 
-    <Nav />
+  return (
+    <div>
+      <Head>
+        <title>Quiz what it is</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <div className="hero">
-      <h1 className="title">Quiz what it is</h1>
-      <p className="description">
-        Personality quizzes for everything!
-      </p>
+      <Nav />
 
-    <div className="row">
-      <ul>
-        <li>What ghost town are you from</li>
-        <li>What Russian author are you</li>
-      </ul>
-    </div>
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+      <div className="hero">
+        <h1 className="title">Quiz what it is</h1>
+        <p className="description">
+          Personality quizzes for everything!
+        </p>
+
+        <div className="row">
+          <ul>
+            {quizLinks}
+          </ul>
+        </div>
       </div>
-    </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+      {style}
+    </div>
+  )
+}
+
+Home.getInitialProps = async ({ req }) => {
+  // we can load stuff from the DB/fs here
+  const response = await fetch(`http://localhost:3000/api/quizzes`)
+  return response.json();
+}
+
+function getQuizUri({id}) {
+  return "./quizzes/" + id
+}
+
+const listPrefix = 'list of'
+const pluralSuffix = 's'
+function getQuizName({title}) {
+  title = title.toLowerCase();
+
+  if (title.startsWith(listPrefix)) {
+    title = title.slice(listPrefix.length + 1)
+  }
+
+  if (title.endsWith(pluralSuffix)) {
+    title = title.slice(0, -pluralSuffix.length)
+  }
+
+  return `What ${title} are you?`
+}
 
 export default Home
+
+const style = <style>{`
+.hero {
+  width: 100%;
+  color: #333;
+}
+.title {
+  margin: 0;
+  width: 100%;
+  padding-top: 80px;
+  line-height: 1.15;
+  font-size: 48px;
+}
+.title,
+.description {
+  text-align: center;
+}
+.row {
+  max-width: 880px;
+  margin: 80px auto 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.card {
+  padding: 18px 18px 24px;
+  width: 220px;
+  text-align: left;
+  text-decoration: none;
+  color: #434343;
+  border: 1px solid #9b9b9b;
+}
+.card:hover {
+  border-color: #067df7;
+}
+.card h3 {
+  margin: 0;
+  color: #067df7;
+  font-size: 18px;
+}
+.card p {
+  margin: 0;
+  padding: 12px 0 0;
+  font-size: 13px;
+  color: #333;
+}
+`}</style>
